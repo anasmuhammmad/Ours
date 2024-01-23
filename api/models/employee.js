b1052b2e32d00ae9626e03caf9de22f126e36be1
Anas <anasmuhammed444@gmail.com>
2024-01-23 18:10:16 +0530

    Employee & Attendance Schema designing

---
+++ api/models/employee.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const employeeSchema = new mongoose.Schema({
+  employeeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  employeeName: {
+    type: String,
+    required: true,
+  },
+  designation: { type: String, required: true },
+  joiningDate: { type: String, required: true },
+  dateOfBirth: { type: String, required: true },
+  salary: { type: Number, required: true },
+  activeEmployees: {
+    type: Boolean,
+    required: true,
+  },
+  phoneNumber: { type: String, type: String },
+  address: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
