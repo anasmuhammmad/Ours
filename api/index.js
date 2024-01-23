a9132b9d83cef07710785e4183d53d36a40a14fa
Anas <anasmuhammed444@gmail.com>
2024-01-23 17:54:32 +0530

    Initial Commit- Connected the Mongodb

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb+srv://anas:anas@cluster0.z7lv85o.mongodb.net/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+app.listen(port , () => {
+    console.log("Server is running on port 8000" );
+})
